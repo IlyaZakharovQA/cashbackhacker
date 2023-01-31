bcf44e940ea7160bd4ec637c2cc14eb4146e95ee
Ilya Zakharov QAMID-50 <oiska@bk.ru>
2023-01-31 19:01:50 +0300

    @Test

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,33 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain200() {
+        int amount = 800;
+        int expected = 200;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals (actual, expected);
+    }
+    @Test
+    public void testRemain1000() {
+        int amount = 1000;
+        int expected = 0;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals (actual, expected);
+    }
+    @Test
+    public void testRemain999() {
+        int amount = 1001;
+        int expected = 999;
+        CashbackHackService service = new CashbackHackService();
+        int actual = service.remain(amount);
+        assertEquals (actual, expected);
+    }
+}
