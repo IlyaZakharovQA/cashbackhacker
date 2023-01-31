4f41c8c7d36d25a0079bb3bb00e5b319f45cff7b
Ilya Zakharov QAMID-50 <oiska@bk.ru>
2023-01-31 20:18:51 +0300

    Correction expected actual

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -12,7 +12,7 @@ public class CashbackHackServiceTest {
         int expected = 300;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
-        assertEquals (actual, expected);
+        assertEquals (expected, actual);
     }
     @Test
     public void testRemain1000() {
@@ -20,7 +20,7 @@ public class CashbackHackServiceTest {
         int expected = 0;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
-        assertEquals (actual, expected);
+        assertEquals (expected, actual);
     }
     @Test
     public void testRemain999() {
@@ -28,6 +28,6 @@ public class CashbackHackServiceTest {
         int expected = 999;
         CashbackHackService service = new CashbackHackService();
         int actual = service.remain(amount);
-        assertEquals (actual, expected);
+        assertEquals (expected, actual);
     }
 }
